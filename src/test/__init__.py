48a8b9fed7bcf5bdb98a8c2fb3450b54d037960e
Sujish Maharjan <sujish.maharjan@codavatar.tech>
2025-12-17 13:21:04 +0545

    chore:[dev] Clear all the files for initial

--- src/test/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.core.handlers import CommandHandlers
-from src.test.handlers import print_any_test
-
-
-test_handler = CommandHandlers("test")
-
-
-test_handler.handler(print_any_test)
--- src/test/commands.py
+++
@@ -1,6 +0,0 @@
-from src.core.commands import Command
-from src.core.handlers import CommandHandlers
-
-
-class PrintAnyCommand(Command):
-    text: str | None = None
